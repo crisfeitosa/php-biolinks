77cd9edc2775f2238ac8681ea0b0bfbb81847233
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-09-14 11:31:38 -0300

    Validação de Formulário

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,31 @@
 <div>
     <h1>Login</h1>
 
+    @if ($message = session()->get('message'))
+        <div>{{ $message }}</div>
+    @endif
+
     <form action="/login" method="post">
         @csrf
-        <input name="email" type="email" value="{{ old('email') }}" placeholder="Email" />
-        <input name="password" type="password" placeholder="Password" />
+
+        <div>
+            <input name="email" type="email" value="{{ old('email') }}" placeholder="Email" />
+            @error('email')
+                <span>{{ $message }}</span>
+            @enderror
+        </div>
+
+        <br />
+
+        <div>
+            <input name="password" type="password" placeholder="Password" />
+            @error('password')
+                <span>{{ $message }}</span>
+            @enderror
+        </div>
+
+        <br />
+
         <button type="submit">Login</button>
     </form>
 </div>
