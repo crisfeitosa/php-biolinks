da972dce9220540680b7589f57178d4da06f6620
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-09-14 20:56:32 -0300

    Registrando um usuário

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,18 +1,33 @@
 <div>
-    <h1>Login</h1>
+    <h1>Register</h1>
 
     @if ($message = session()->get('message'))
         <div>{{ $message }}</div>
     @endif
 
-    <form action="{{ route('login') }}" method="post">
+    <form action="{{ route('register') }}" method="post">
         @csrf
+
+        <div>
+            <input name="name" value="{{ old('name') }}" placeholder="Name" />
+            @error('name')
+                <span>{{ $message }}</span>
+            @enderror
+        </div>
+
+        <br />
 
         <div>
             <input name="email" type="email" value="{{ old('email') }}" placeholder="Email" />
             @error('email')
                 <span>{{ $message }}</span>
             @enderror
+        </div>
+
+        <br />
+
+        <div>
+            <input name="email_confirmation" placeholder="Email Confirmation" />
         </div>
 
         <br />
@@ -26,6 +41,6 @@
 
         <br />
 
-        <button type="submit">Login</button>
+        <button type="submit">Register</button>
     </form>
 </div>
