ebd5ef91638f5438d72839040ea24b48c536f9e2
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-09-15 17:59:36 -0300

    Criando um Link

---
+++ resources/views/links/create.blade.php
@@ -0,0 +1,32 @@
+<div>
+    {{ auth()->id()}}
+    <h1>Create a Link</h1>
+
+    @if ($message = session()->get('message'))
+        <div>{{ $message }}</div>
+    @endif
+
+    <form action="{{ route('links.create') }}" method="post">
+        @csrf
+
+        <div>
+            <input name="link" value="{{ old('link') }}" placeholder="New Link" />
+            @error('link')
+                <span>{{ $message }}</span>
+            @enderror
+        </div>
+
+        <br />
+
+        <div>
+            <input name="name" value="{{ old('name') }}" placeholder="Name" />
+            @error('name')
+                <span>{{ $message }}</span>
+            @enderror
+        </div>
+
+        <br />
+
+        <button type="submit">Save</button>
+    </form>
+</div>
