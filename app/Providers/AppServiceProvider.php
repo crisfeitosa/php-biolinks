b6ccfaedc586a98b68e2d22e89bdda483331853e
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-09-14 21:41:50 -0300

    Service Providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::unguard();
+        Password::defaults(function(){
+
+            $rule = Password::min(8);
+
+            return $this->app->isProduction()
+            ? $rule->mixedCase()->uncompromised() : $rule;
+        });
     }
 }
