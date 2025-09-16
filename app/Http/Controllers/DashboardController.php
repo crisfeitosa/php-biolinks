38905cbcb19603ce4e97ed82009cd1a287bac3de
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-09-16 19:49:39 -0300

    Instalando Laravel DebugBar

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,7 +10,7 @@ class DashboardController extends Controller
     public function __invoke() {
         $user = Auth::user();
 
-        dd( $user->links );
+        dump( $user->links );
 
         return view('dashboard');
     }
