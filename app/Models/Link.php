cbbb8a29810e77137994fcc82c7b22ec24246d62
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-09-17 15:52:48 -0300

    Refatorando ordenação para dentro do model

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -9,4 +9,30 @@ class Link extends Model
 {
     /** @use HasFactory<\Database\Factories\LinkFactory> */
     use HasFactory;
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function moveDown(){
+        $this->move(+1);
+    }
+
+    public function moveUp(){
+        $this->move(-1);
+    }
+
+    private function move($to) {
+        $order = $this->sort;
+        $newOrder = $order + $to;
+
+        $user = $this->user;
+
+        $swapWith = $user->links()
+            ->where('sort', '=', $newOrder)
+            ->first();
+
+        $this->fill(['sort' => $newOrder])->save();
+        $swapWith->fill(['sort' => $order])->save();
+    }
 }
